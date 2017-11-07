9a8aad2888143fdd12457a79cad8cbe11966d91e
Owner <Owner@Bjorn-Laptop>
2017-11-06 18:41:15 -0800

    Finalized changes, need to test code for climb and autonomous

--- SteamWorks2017CompetitionCode/src/org/usfirst/frc/team3482/robot/Robot.java
+++ SteamWorks2017CompetitionCode/src/org/usfirst/frc/team3482/robot/Robot.java
@@ -1,5 +1,4 @@
 package org.usfirst.frc.team3482.robot;
-
 //import org.opencv.core.Core;
 //import org.opencv.core.Mat;
 //import org.opencv.core.Point;
@@ -22,13 +21,6 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
 //	UsbCamera cam;
 	public static double shooterSpeed = 0.0;
@@ -49,12 +41,14 @@ public class Robot extends IterativeRobot {
 		gearManipulator = new GearManipulator();
 		chassis = new Chassis();
 		
-		autoChooser = new SendableChooser<>();
-		autoChooser.addDefault("Default Auto", null);
-		autoChooser.addObject("Select if on the side positions", new AutoSide());
-		autoChooser.addObject("Select if we start middle position", new AutoMiddle());
+		autoChooser = new SendableChooser<Command>();
+		autoChooser.addDefault("No Auto", null);
+		autoChooser.addObject("Side Auto", new AutoSide());
+		autoChooser.addObject("Middle Auto", new AutoMiddle());
 		SmartDashboard.putData("Auto mode", autoChooser);
+		
 		gearManipulator.moveGearManipReadyPos();
+		
 //		new Thread(() -> {
 //			cam = CameraServer.getInstance().startAutomaticCapture(0);
 //			cam.setBrightness(30);
@@ -79,30 +73,16 @@ public class Robot extends IterativeRobot {
 //		}).start();
 	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
 	public void disabledInit() {
 		
 	}
+	
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
+
 	public void autonomousInit() {
-		autonomousCommand = new AutoMiddle();
+		autonomousCommand = (Command) autoChooser.getSelected();
 		if (autonomousCommand != null){
 			autonomousCommand.start();
 		}
@@ -116,15 +96,14 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void teleopInit() {
-		
-		
 		if (autonomousCommand != null)
 			autonomousCommand.cancel();
+		shooterSpeed=(.6); //range -.5 to -.833
 	}
 	
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
-		shooterSpeed=(.6); //range -.5 to -.833
+		
 		
 		SmartDashboard.putNumber("Current Shooter Percentage: ", shooterSpeed);
 		SmartDashboard.putNumber("Gear Manipulator Position: ", Robot.gearManipulator.getGearManipPosition());
